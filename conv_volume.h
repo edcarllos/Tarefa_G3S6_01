e26d51e15743441bdbecda873fce131d8f16ba24
David Rios <55660690+davidrioss@users.noreply.github.com>
2024-12-20 11:02:48 -0300

    função que converte metros cúbicos para mililitros

--- conv_volume.h
+++ conv_volume.h
@@ -186,7 +186,22 @@ void conv_mililitros_para_metros_cubicos(){
  *  
  */
 void conv_metros_cubicos_para_mililitros(){
+    double valor, valor_convertido;
 
+    printf("\a\nDigite o valor a ser convertido: ");
+    scanf("%lf", &valor);
+
+    if (valor >= 0)
+    {
+        valor_convertido = valor * 1000000;
+
+        printf("\a\n%.2lf metros cúbicos equivalem a %.0lf mililitros.\n", valor, valor_convertido);
+    }
+    else
+    {
+        printf("\a\nO valor precisa ser um numero positivo.");
+    }
+    
 }
 
 /*
