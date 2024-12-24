5bc1c1aa6e594ab95f351afe25d052acbc033b5e
Andre <andrelimatecnologias@gmail.com>
2024-12-24 16:58:13 -0300

    declaração das funções dentro do arquivo de cabeçalho

---
+++ src/conv_massa.h
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+void interface_conv_massa();
+
+double conv_grama_para_quilo(double g);
+
+double conv_quilo_para_grama(double q);
+
+double conv_quilo_para_tonelada(double q);
+
+double conv_tonelada_para_quilo(double t);
+
+double conv_grama_para_tonelada(double g);
+
+double conv_tonelada_para_grama(double t);
+
