fdf191551bc7c0d25ed651ab39cdc60b19fc0d6a
SrJamees <jamesrsxavier99@gmail.com>
2024-12-24 16:10:42 -0300

    Implementação da Conversao de tempo

---
+++ src/conv_tempo.h
@@ -0,0 +1,12 @@
+#ifndef CONV_TEMPO_H
+#define CONV_TEMPO_H
+
+// Declarações das funções de conversão de tempo
+double segundos_para_minutos(double segundos);
+double segundos_para_horas(double segundos);
+double minutos_para_segundos(double minutos);
+double minutos_para_horas(double minutos);
+double horas_para_segundos(double horas);
+double horas_para_minutos(double horas);
+
+#endif // CONV_TEMPO_H
